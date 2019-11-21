553890bd0e575551188f2f750bf51597462a85eb
ThamiduAnjana <46102435+ThamiduAnjana@users.noreply.github.com>
2019-11-22 00:07:41 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>POST WEB</title>
+</head>
+<body>
+
+<form method="POST" action="POSTPHP.php">
+	<h3>
+		Name : 
+	</h3>
+	<input type="text" name="name" placeholder="Enter Your Name" style="width: 25rem;height: 2rem;">
+	<br><br>
+	<input type="submit" name="btn_sub" value="Submit">
+</form>
+
+</body>
+</html>
